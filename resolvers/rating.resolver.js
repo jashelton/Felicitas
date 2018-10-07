d69b337c6ba0487729c62ec871afb445b4089ce8
jashelton <justin.shelton@yahoo.com>
2018-10-07 03:01:42 -0400

    fix issue where avg_rating wouldnt update

--- resolvers/rating.resolver.js
+++ resolvers/rating.resolver.js
@@ -18,7 +18,20 @@ export default {
         await models.Rating.create({ event_id, user_id: user.id, value });
       }
 
-      return models.Event.findById(event_id);
+      // TODO: This is gross but needs for now since I change avg_rating in the event.resolver
+      const event = await models.Event.findById(event_id);
+      const avg_rating = await models.Rating.findAll({
+        where: { event_id },
+        attributes: [
+          [
+            models.sequelize.fn("AVG", models.sequelize.col("value")),
+            "avg_rating"
+          ]
+        ]
+      });
+
+      event.dataValues.avg_rating = avg_rating[0].dataValues.avg_rating;
+      return event.get({ plain: true });
     }
   }
 };
